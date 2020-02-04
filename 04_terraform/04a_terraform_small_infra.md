781ed1cdbadcb16fd9e0c1820956abc43367de09
Pierre Gaxatte <pierre.gaxatte@corp.ovh.com>
2020-02-04 09:39:05 +0100

    WIP attach volumes

--- 04_terraform/04a_terraform_small_infra.md
+++ 04_terraform/04a_terraform_small_infra.md
@@ -4,3 +4,18 @@ export TERRAFORM_VERSION=0.12.20
 curl -Lo - https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip | gunzip -d - > /usr/local/bin/terraform
 chmod +x /usr/local/bin/terraform
 ```
+
+Initialize the terraform plugins and state:
+```
+terraform init
+```
+
+Deploy the infrastructure
+```
+terraform apply
+```
+
+Destroy it all:
+```
+terraform destroy
+```
--- 04_terraform/main.tf
+++ 04_terraform/main.tf
@@ -11,6 +11,11 @@ variable nb {
   default     = 3
 }
 
+data "openstack_networking_network_v2" "ext_net" {
+  name      = "Ext-Net"
+  tenant_id = ""
+}
+
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
@@ -19,11 +24,6 @@ resource "tls_private_key" "private_key" {
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = "tf-keypair"
   public_key = tls_private_key.private_key.public_key_openssh
-}
-
-data "openstack_networking_network_v2" "ext_net" {
-  name      = "Ext-Net"
-  tenant_id = ""
 }
 
 resource "openstack_networking_port_v2" "public_port" {
@@ -46,6 +46,19 @@ resource "openstack_compute_instance_v2" "node" {
   lifecycle {
     ignore_changes = [user_data, image_id, key_pair]
   }
+}
+
+resource "openstack_blockstorage_volume_v2" "vol_node" {
+  count       = var.nb
+  name        = "tf-vol-${format("%03d", count.index)}"
+  size        = 5
+}
+
+resource "openstack_compute_volume_attach_v2" "vol_node_attach" {
+  count = var.nb
+
+  instance_id = openstack_compute_instance_v2.node[count.index].id
+  volume_id   = openstack_blockstorage_volume_v2.vol_node[count.index].id
 }
 
 output keypair_priv {
